95d0eca214e3426825ebd405bb98b4d5d992c6df
ravanton <antonrekhva@gmail.com>
2022-09-11 15:49:16 +0300

    Print email

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -20,6 +20,7 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#email-detail-view').style.display = 'none';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -27,11 +28,30 @@ function compose_email() {
   document.querySelector('#compose-body').value = '';
 }
 
+function view_email(id) {
+  fetch(`/emails/${id}`)
+  .then(response => response.json())
+  .then(email => {
+      // Print email
+      console.log(email);
+      document.querySelector('#emails-view').style.display = 'none';
+      document.querySelector('#compose-view').style.display = 'none';
+      document.querySelector('#email-detail-view').style.display = 'block';
+
+
+      // ... do something else with email ...
+      document.querySelector('#email-detail-view').innerHTML = `
+      HI
+      `;
+  });
+}
+
 function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#email-detail-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
@@ -59,8 +79,8 @@ function load_mailbox(mailbox) {
         newEmail.className = singleEmail.read ? 'read': 'unread';
 
         // Add click event to view email
-        newEmail.addEventListener('click', function() {
-          console.log('This newEmail has been clicked!')
+        newEmail.addEventListener('click', function (){
+          view_email(singleEmail.id)
         });
         document.querySelector('#emails-view').append(newEmail);
       })
