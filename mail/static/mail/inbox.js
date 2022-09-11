e3a615b59e7f9813138625d91d5e4055be37d14f
ravanton <antonrekhva@gmail.com>
2022-09-11 14:32:42 +0300

    Fixed mailbox

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -35,8 +35,33 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
-}
 
+
+  //  Get the email for that mailbox and user
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+      // Loop through email and create a div for each 
+      emails.forEach(singleEmail => {
+
+        console.log(singleEmail);
+
+        // Create  div for each email 
+        const newEmail = document.createElement('div');
+        newEmail.innerHTML = `
+        <h3>Sender: ${singleEmail.sender}</h3>
+        <h4>Subject: ${singleEmail.subject}</h4>
+        <p>${singleEmail.timestamp}</p>
+
+        
+        `;
+        newEmail.addEventListener('click', function() {
+          console.log('This newEmail has been clicked!')
+        });
+        document.querySelector('#emails-view').append(newEmail);
+      })
+  });
+  } 
 function send_email(event) {
   event.preventDefault();
 
@@ -59,30 +84,5 @@ function send_email(event) {
       // Print result
       console.log(result);
       load_mailbox('sent');
-  });
-
-  //  Get the email for that mailbox and user
-  fetch(`/emails/${mailbox}`)
-  .then(response => response.json())
-  .then(emails => {
-      // Loop through email and create a div for each 
-      emails.forEach(singleEmail => {
-
-        console.log(singleEmail);
-
-        // Create  div for each email 
-        const newEmail = document.createElement('div');
-        newEmail.innerHTML = `
-        <h3>Sender: ${singleEmail.sender}</h3>
-        <h4>Subject: ${singleEmail.subject}</h4>
-        <p>${singleEmail.timestamp}</p>
-
-        
-        `;
-        newEmail.addEventListener('click', function() {
-           console.log('This newEmail has been clicked!')
-        });
-        document.querySelector('#emails-view').append(newEmail);
-      })
   });
 }
